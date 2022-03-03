9ad94696fa0abe96864d8c7ebfcb53dfc3a551be
syctis <68449569+syctis@users.noreply.github.com>
2022-03-03 22:59:22 +0100

    Create index.html for Wehling_Beta GitHub Pages

---
+++ contact_config.php
@@ -0,0 +1,4 @@
+<?php
+// To
+define("WEBMASTER_EMAIL", 'wehling.mieder-waesche@web.de');
+?>
---
+++ js/slider.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+	$('.partner-logo').slick({
+		slidesToShow: 4,
+		slidesToScroll: 1;
+		autoplay: true;
+		autoplaySpeed: 1500;
+		arrows: false,
+		dots: false,
+		pauseOnHover: false,
+		responsive: [{
+			breakpoint: 768,
+			settings: {
+				slidesToShow: 2
+				}
+		}, {breakpoint: 520,
+		settings: {slidesToShow: 3
+		}
+		}]
+	});
+});
